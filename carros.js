ab6c74c3179b0a43bd5526e562efe0feb1c1f710
Yves Botshimbo Luana <ybidotsh@gmail.com>
2021-08-14 02:56:39 -0300

    Primeiro commit

--- carros.js
+++ carros.js
@@ -1,29 +1,50 @@
 module.exports.temCarroDaMarca = function temCarroDaMarca(carros, marca) {
   // coloque aqui o algoritmo para retornar um boolean (true ou false)
   // se existir um carro com a marca informada ou não
-  return null;
+
+  let carros = [];
+    if (temCarroDaMarca = "fiat") {
+      const carros = { carros: carros.marca }
+      carrosFiat.includes(carrosFiat)
+  }  
+   return null;
 }
 
 module.exports.carrosNaCor = function carrosNaCor(carros, cor) {
   // coloque aqui o algoritmo para retornar a quantidade
   // de carros na cor recebida como parâmetro
+
+  let quantidadecarrosNaCor = 0;
+    if (carros.carrosNaCor === "cor") {
+      quantidadecarrosNaCor++
+      }
   return null;
 }
 
 module.exports.anosDisponiveis = function anosDisponiveis(carros) {
   // coloque aqui o algoritmo para retornar um array de number
   // com os anos disponíveis nos carros informados
-  return null;
+
+  let anosDisponiveis = [];
+    anosDisponiveis.length
+     return null;
 }
 
 module.exports.tiposDisponiveis = function tiposDisponiveis(carros) {
   // coloque aqui o algoritmo para retornar um array de strings
   // com os tipos disponíveis nos carros informados
+
+  let tiposDisponiveis = [];
+    typeof tiposDisponiveis 
   return null;
 }
 
 module.exports.selecionarCarrosDaMarca = function selecionarCarrosDaMarca(carros, marca) {
   // coloque aqui o algoritmo para retornar um array de object
   // com os carros da marca informado como parâmetro
+
+  let selecionarCarrosDaMarca = {};
+    Object.selecionarCarrosDaMarca
+    
   return null;
 }
