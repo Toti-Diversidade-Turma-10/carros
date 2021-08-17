d771a6e4c1f79e47d0fda70925e3c23069f325a4
Vanessa Berdayes <berdayesvane@gmail.com>
2021-08-17 18:05:14 -0300

    Adicionando algoritmos que passaram nos testes

--- carros.js
+++ carros.js
@@ -1,32 +1,89 @@
 function temCarroDaMarca(carros, marca) {
+  for (let carro of carros){
+    if (carro.marca === marca) {
+      return true;
+    }
+  }
+  return false;
+}
   // coloque aqui o algoritmo para retornar um boolean (true ou false)
   // se existir um carro com a marca informada ou não
-  return null;
-}
+
+
 
 function carrosNaCor(carros, cor) {
+ 
+ for (let carro of carros){
+  if (carro.cor !== cor) {
+    return 0;
+   }
+ }
+ 
+
+ 
+    let quantidade = 0;
+
+    for (let carro of carros) {
+      if (carro.cor === cor) {
+        quantidade++
+      }
+    }
+    return quantidade;
+}
+
   // coloque aqui o algoritmo para retornar a quantidade
   // de carros na cor recebida como parâmetro
-  return null;
-}
+
+
 
 function anosDisponiveis(carros) {
+let array = [];
+
+  for (let carro of carros) {
+  if (!array.includes(carro.ano)){
+    array.push(carro.ano)
+  }   
+    }
+      return array;
+}
+
   // coloque aqui o algoritmo para retornar um array de number
   // com os anos disponíveis nos carros informados
-  return null;
-}
+ 
+
 
 function tiposDisponiveis(carros) {
-  // coloque aqui o algoritmo para retornar um array de strings
-  // com os tipos disponíveis nos carros informados
-  return null;
+
+ let array = [];
+
+ for (let carro of carros) {
+   if (!array.includes(carro.tipo)){
+    array.push(carro.tipo)
+   }
+ }
+ return array;
 }
 
+  // coloque aqui o algoritmo para retornar um array de strings
+  // com os tipos disponíveis nos carros informados
+
+
+
 function selecionarCarrosDaMarca(carros, marca) {
+
+let array = [];
+
+for (let carro of carros) {
+  if (carro.marca === marca) {
+    array.push(carro)
+  }
+}
+return array;
+}
+
   // coloque aqui o algoritmo para retornar um array de object
   // com os carros da marca informado como parâmetro
-  return null;
-}
+
 
 module.exports = {
   anosDisponiveis,
