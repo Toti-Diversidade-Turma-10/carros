730b70affc902753e2f78f383ff877b8025edc31
Yves Botshimbo Luana <ybidotsh@gmail.com>
2021-08-17 18:51:14 -0300

    Segundo commit

--- carros.js
+++ carros.js
@@ -2,49 +2,83 @@ module.exports.temCarroDaMarca = function temCarroDaMarca(carros, marca) {
   // coloque aqui o algoritmo para retornar um boolean (true ou false)
   // se existir um carro com a marca informada ou não
 
-  let carros = [];
-    if (temCarroDaMarca = "fiat") {
-      const carros = { carros: carros.marca }
-      carrosFiat.includes(carrosFiat)
-  }  
-   return null;
+  if (carros.length === 0) {
+    return false;
+  }
+
+  for (let carro of carros) {
+    if (carro.marca === marca) {
+      return true;
+    }
+  }
+
+  return false;
 }
+  
 
 module.exports.carrosNaCor = function carrosNaCor(carros, cor) {
   // coloque aqui o algoritmo para retornar a quantidade
   // de carros na cor recebida como parâmetro
 
-  let quantidadecarrosNaCor = 0;
-    if (carros.carrosNaCor === "cor") {
-      quantidadecarrosNaCor++
+  const retorno = 0;
+
+    for (let carro of carros) {
+      if (carros.cor === cor) {
+        quantidadeDeCarroNaCorRecebido ++
+      
       }
-  return null;
+    }
+  return retorno
 }
 
 module.exports.anosDisponiveis = function anosDisponiveis(carros) {
   // coloque aqui o algoritmo para retornar um array de number
   // com os anos disponíveis nos carros informados
 
-  let anosDisponiveis = [];
-    anosDisponiveis.length
-     return null;
+  const retorno = [];
+
+  for (let carro of carros) {
+    if (!retorno.includes(carro.ano)) {
+    retorno.push(carro.ano);
+    }
+  }
+  return retorno;
 }
 
 module.exports.tiposDisponiveis = function tiposDisponiveis(carros) {
   // coloque aqui o algoritmo para retornar um array de strings
   // com os tipos disponíveis nos carros informados
 
-  let tiposDisponiveis = [];
-    typeof tiposDisponiveis 
-  return null;
+  const retorno = [""];
+    
+      if (carro.length === 0) {
+        return resultado;
+      }
+      for (carro of carros) {
+        if(retorno.length(carro.tipo)){
+        return.push(carro.tipo);
+        }
+
+      }
+  
+  return retorno;
 }
 
 module.exports.selecionarCarrosDaMarca = function selecionarCarrosDaMarca(carros, marca) {
   // coloque aqui o algoritmo para retornar um array de object
   // com os carros da marca informado como parâmetro
 
-  let selecionarCarrosDaMarca = {};
-    Object.selecionarCarrosDaMarca
+  const retorno = {};
+
+   for (let carro of carros)
+    if (carro.marca === marca) {
+      return retorno;
+    }
+
+    else if (carro.marca === marca)) {
+      return retorno
+      
+    }
     
-  return null;
+    return retorno;
 }
